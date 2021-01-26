3f0e6217b908b24b4215cb8c689f9a3b173d86e5
jeshvenkatesh <venkateswara.kolli@talview.com>
2021-01-26 11:54:58 +0530

    initial commit

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,19 @@
-$(document).ready(()=>{
-    $('button').click(()=>{
-        $('p').hide();
-    })
-})
+// $(document).ready(()=>{
+//     $('button').click(()=>{
+//         // $("p").hide(1000)  //  slow, fast, milliseconds
+//         $('p').toggle('slow');
+//     })
+// });
+// $(window).load(function(){
+//     $('#div1').html('Height =' + $('#img1').height() + "<br/>" + "Width =" + $('#img1').width())
+// });
+
+  $(document).ready(function() {
+    alert("Dom Loaded.");
+  });
+
+  $(window).on("load", function () {
+    alert("Window Loaded.");
+  }); 
+
+  
